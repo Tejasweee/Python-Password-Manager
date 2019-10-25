a9a7bd8bb84305e6c31c1433ba6f1957b319754a
Tejasweee <56251303+Tejasweee@users.noreply.github.com>
2019-10-25 10:26:59 +0545

    Update pwdm.py

--- pwdm.py
+++ pwdm.py
@@ -95,12 +95,12 @@ def retordel(value, secret_key, master_pwd):
                 comp= dataret[0]
                 try:
                     repass= suite.decrypt(passenc)
-                    print('Password of ' + comp+ ' is:')
+                    print('Password of ' + comp+ ' (ID: '+ str(num)+ ')' ' is:')
                     print(repass.decode())
                     print('-'*42)
                 except Exception as e:
                     print(e)
-                    print('This password was encrypted using different master password. Try entering correct master password')
+                    print('This password was encrypted using different master password. Try joining the session with correct master password.')
 
             if value == 3:
                 with conn:
@@ -108,10 +108,42 @@ def retordel(value, secret_key, master_pwd):
                 print('Successfully deleted ' + 'ID ' +str(num)+ ' from the database')
                 print('')
 
+def checkmasterpwd(secret_key, master_pwd):
+    curr.execute("SELECT id, choosen,key FROM pwdb_table")
+    passwithid = curr.fetchall()
+    if len(passwithid)>0:
+        passwithid= passwithid[-1]
+        passenc= passwithid[2]
+        reqdpass=passwithid[1].split(',')
+        a,b,c,d= int(reqdpass[0]), int(reqdpass[1]),int(reqdpass[2]),int(reqdpass[3])
+        cryp_key=secret_key[a:a+8]+master_pwd+secret_key[b:b+8]+master_pwd+secret_key[c:c+8]+master_pwd+secret_key[d:d+8]+ master_pwd
+        cryp_key= cryp_key[0:32]
+        cryp_key= base64.urlsafe_b64encode((cryp_key).encode())
+        suite=Fernet(cryp_key)
+        try:
+            repass= suite.decrypt(passenc)
+            print('Session joined successfully...')
+        except:
+            print('')
+            print('WARNING! This master password you entered can not decrypt the last password previously saved here!')
+            print('Using different master passwords is fine but it may cause conflicts! ')
+            print('')
+            print('Do you want to continue with this different master password? y/n ')
+            print('Enter y if you want to continue with this master password, enter n to enter previously used master password.')
+            print('')
+            usr=input().lower()
+            if usr!='y':
+                print('Enter correct master password in the next session...')
+                sys.exit()
+            else:
+                print('Session continued with different master password than previously entered...')
+
 def showdatabase():
+    '''prints all values stored inside sqlite database present in pwdb.db'''
     curr.execute("SELECT id, choosen,company,date,key FROM pwdb_table")
     passwithid = curr.fetchall()
-    
+    print('DATABASE: ')
+    print(' ')
     for ids in passwithid:
         print(str(ids[0])+', '+ str(ids[2]) +', '+ str(ids[3]) +', ' +str(ids[1]) +', '+ str(ids[4]))
         print('_'*85)
@@ -134,8 +166,10 @@ conn = sqlite3.connect('pwdb.db')
 curr= conn.cursor()
 
 def loop(secret_key, master_pwd):
-    '''Loops continously generating a session of the entered master password, exits if value of 4 or greater than 4 provided.'''
+    '''Loops continously generating a session of the entered master password, exits if value of 5 or greater than 5 provided.'''
     print('')
+    print('_'*20)
+    print('SESSION CONTINUED:')
     print('_'*85)
     print('1- store password')
     print('2- retrieve password')
@@ -164,12 +198,12 @@ def loop(secret_key, master_pwd):
         loop(secret_key, master_pwd)
 
 if __name__ == "__main__":
-    print('Welcome to Python Password Manager')
+    print('Welcome to Python Password Manager!')
     print('_'*85)
     if len(sys.argv)>1:
         master_pwd= sys.argv[1]
     else:
         master_pwd=input("Enter master passord: ")
-    print('TO make sure that you entered the correct master password, you can retrieve one of previously saved password (if any) to avoid conflicts of different master passwords.')
     secret_key=getsecretkey(master_pwd)
+    checkmasterpwd(secret_key, master_pwd)
     loop(secret_key, master_pwd)
